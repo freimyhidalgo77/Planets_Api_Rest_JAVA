d93f297360e1ea8ddd5ee5e386e9c3e1e5c8b3ac
freimyHidalgo77 <freimy4@gmail.com>
2026-02-23 11:18:19 -0400

    ResourceNotFoundException

--- src/main/java/com/example/Api_Rest_JAVA_SpringBoot/exeption/ResourceNotFoundException.java
+++ src/main/java/com/example/Api_Rest_JAVA_SpringBoot/exeption/ResourceNotFoundException.java
@@ -3,5 +3,6 @@ package com.example.Api_Rest_JAVA_SpringBoot.exeption;
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
+
     }
 }
