218a8b3a48502f0efd433cd71ed0948050c8198a
freimyHidalgo77 <freimy4@gmail.com>
2026-02-23 10:28:24 -0400

    Response

--- src/main/java/com/example/Api_Rest_JAVA_SpringBoot/dto/common/Response.java
+++ src/main/java/com/example/Api_Rest_JAVA_SpringBoot/dto/common/Response.java
@@ -1,4 +1,20 @@
 package com.example.Api_Rest_JAVA_SpringBoot.dto.common;
 
-public class Response {
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.logging.log4j.message.Message;
+
+public class Response<T> {
+
+    private boolean success;
+    private String Message;
+    private T data;
+
+    public Response(boolean success, String Message, T data){
+        this.success = success;
+        this.Message = Message;
+        this.data = data;
+
+    }
 }
+
